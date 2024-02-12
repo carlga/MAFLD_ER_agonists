dc94d56e5678a5045a90f30ee5672b5ed6855efb
Christian Sommerauer <57133047+ChrisSomm@users.noreply.github.com>
2024-02-12 08:57:00 +0100

    few changes to figures

--- code/05_epigenome_analysis/Epigenome_05.1_computematrices_deepTools.sh
+++ code/05_epigenome_analysis/Epigenome_05.1_computematrices_deepTools.sh
@@ -9,25 +9,25 @@ out_path="results/Epigenome_analysis"
   #For the promoters
  computeMatrix reference-point \
  --referencePoint center -S \
- ${bigwig_path}/231127_CK0734_Input_CD9_RPGC_norm.bw \
- ${bigwig_path}/231127_CK0744_H3K27ac_CD2_RPGC_norm.bw \
- ${bigwig_path}/231127_CD6_H3K27ac_S2_R1_001_RPGC_norm.bw \
- ${bigwig_path}/231127_CK0745_H3K27ac_CD9_RPGC_norm.bw \
- ${bigwig_path}/231127_CK0746_H3K27ac_HFD3_RPGC_norm.bw \
- ${bigwig_path}/231127_CK0747_H3K27ac_HFD4_RPGC_norm.bw \
- ${bigwig_path}/231127_HFD6_H3K27ac_S8_R1_001_RPGC_norm.bw \
- ${bigwig_path}/231127_CK0748_H3K27ac_DPN2_RPGC_norm.bw \
- ${bigwig_path}/231127_CK0749_H3K27ac_DPN3_RPGC_norm.bw \
- ${bigwig_path}/231127_DPN6_H3K27ac_S15_R1_001_RPGC_norm.bw \
- ${bigwig_path}/231127_DIP3_H3K27ac_S9_R1_001_RPGC_norm.bw \
- ${bigwig_path}/231127_DIP6_H3K27ac_S10_R1_001_RPGC_norm.bw \
- ${bigwig_path}/231127_DIP10_H3K27ac_S11_R1_001_RPGC_norm.bw \
- ${bigwig_path}/231127_E2_2_H3K27ac_S16_R1_001_RPGC_norm.bw \
- ${bigwig_path}/231127_CK0750_H3K27ac_E2_8_RPGC_norm.bw \
- ${bigwig_path}/231127_CK0751_H3K27ac_E2_9_RPGC_norm.bw \
- ${bigwig_path}/231127_PPT1_H3K27ac_S12_R1_001_RPGC_norm.bw \
- ${bigwig_path}/231127_PPT2_H3K27ac_S13_R1_001_RPGC_norm.bw \
- ${bigwig_path}/231127_PPT3_H3K27ac_S14_R1_001_RPGC_norm.bw \
+ ${bigwig_path}/CK0734_Input_CD9_RPGC_norm.bw \
+ ${bigwig_path}/CK0744_H3K27ac_CD2_RPGC_norm.bw \
+ ${bigwig_path}/CD6_H3K27ac_S2_R1_001_RPGC_norm.bw \
+ ${bigwig_path}/CK0745_H3K27ac_CD9_RPGC_norm.bw \
+ ${bigwig_path}/CK0746_H3K27ac_HFD3_RPGC_norm.bw \
+ ${bigwig_path}/CK0747_H3K27ac_HFD4_RPGC_norm.bw \
+ ${bigwig_path}/HFD6_H3K27ac_S8_R1_001_RPGC_norm.bw \
+ ${bigwig_path}/CK0748_H3K27ac_DPN2_RPGC_norm.bw \
+ ${bigwig_path}/CK0749_H3K27ac_DPN3_RPGC_norm.bw \
+ ${bigwig_path}/DPN6_H3K27ac_S15_R1_001_RPGC_norm.bw \
+ ${bigwig_path}/DIP3_H3K27ac_S9_R1_001_RPGC_norm.bw \
+ ${bigwig_path}/DIP6_H3K27ac_S10_R1_001_RPGC_norm.bw \
+ ${bigwig_path}/DIP10_H3K27ac_S11_R1_001_RPGC_norm.bw \
+ ${bigwig_path}/E2_2_H3K27ac_S16_R1_001_RPGC_norm.bw \
+ ${bigwig_path}/CK0750_H3K27ac_E2_8_RPGC_norm.bw \
+ ${bigwig_path}/CK0751_H3K27ac_E2_9_RPGC_norm.bw \
+ ${bigwig_path}/PPT1_H3K27ac_S12_R1_001_RPGC_norm.bw \
+ ${bigwig_path}/PPT2_H3K27ac_S13_R1_001_RPGC_norm.bw \
+ ${bigwig_path}/PPT3_H3K27ac_S14_R1_001_RPGC_norm.bw \
   -R ${prom_enh_path}/promoters_HFDup_70_anno.bed \
   ${prom_enh_path}/promoters_HFDdown_72_anno.bed \
   -b 2000 -a 2000 \
@@ -39,25 +39,25 @@ gunzip ${out_path}/DB_H3K27ac_up_down_142prom_2K_R.import.gz
   # For the enhancers
   computeMatrix reference-point \
   --referencePoint center -S \
-  ${bigwig_path}/231127_CK0734_Input_CD9_RPGC_norm.bw \
-  ${bigwig_path}/231127_CK0744_H3K27ac_CD2_RPGC_norm.bw \
-  ${bigwig_path}/231127_CD6_H3K27ac_S2_R1_001_RPGC_norm.bw \
-  ${bigwig_path}/231127_CK0745_H3K27ac_CD9_RPGC_norm.bw \
-  ${bigwig_path}/231127_CK0746_H3K27ac_HFD3_RPGC_norm.bw \
-  ${bigwig_path}/231127_CK0747_H3K27ac_HFD4_RPGC_norm.bw \
-  ${bigwig_path}/231127_HFD6_H3K27ac_S8_R1_001_RPGC_norm.bw \
-  ${bigwig_path}/231127_CK0748_H3K27ac_DPN2_RPGC_norm.bw \
-  ${bigwig_path}/231127_CK0749_H3K27ac_DPN3_RPGC_norm.bw \
-  ${bigwig_path}/231127_DPN6_H3K27ac_S15_R1_001_RPGC_norm.bw \
-  ${bigwig_path}/231127_DIP3_H3K27ac_S9_R1_001_RPGC_norm.bw \
-  ${bigwig_path}/231127_DIP6_H3K27ac_S10_R1_001_RPGC_norm.bw \
-  ${bigwig_path}/231127_DIP10_H3K27ac_S11_R1_001_RPGC_norm.bw \
-  ${bigwig_path}/231127_E2_2_H3K27ac_S16_R1_001_RPGC_norm.bw \
-  ${bigwig_path}/231127_CK0750_H3K27ac_E2_8_RPGC_norm.bw \
-  ${bigwig_path}/231127_CK0751_H3K27ac_E2_9_RPGC_norm.bw \
-  ${bigwig_path}/231127_PPT1_H3K27ac_S12_R1_001_RPGC_norm.bw \
-  ${bigwig_path}/231127_PPT2_H3K27ac_S13_R1_001_RPGC_norm.bw \
-  ${bigwig_path}/231127_PPT3_H3K27ac_S14_R1_001_RPGC_norm.bw \
+  ${bigwig_path}/CK0734_Input_CD9_RPGC_norm.bw \
+  ${bigwig_path}/CK0744_H3K27ac_CD2_RPGC_norm.bw \
+  ${bigwig_path}/CD6_H3K27ac_S2_R1_001_RPGC_norm.bw \
+  ${bigwig_path}/CK0745_H3K27ac_CD9_RPGC_norm.bw \
+  ${bigwig_path}/CK0746_H3K27ac_HFD3_RPGC_norm.bw \
+  ${bigwig_path}/CK0747_H3K27ac_HFD4_RPGC_norm.bw \
+  ${bigwig_path}/HFD6_H3K27ac_S8_R1_001_RPGC_norm.bw \
+  ${bigwig_path}/CK0748_H3K27ac_DPN2_RPGC_norm.bw \
+  ${bigwig_path}/CK0749_H3K27ac_DPN3_RPGC_norm.bw \
+  ${bigwig_path}/DPN6_H3K27ac_S15_R1_001_RPGC_norm.bw \
+  ${bigwig_path}/DIP3_H3K27ac_S9_R1_001_RPGC_norm.bw \
+  ${bigwig_path}/DIP6_H3K27ac_S10_R1_001_RPGC_norm.bw \
+  ${bigwig_path}/DIP10_H3K27ac_S11_R1_001_RPGC_norm.bw \
+  ${bigwig_path}/E2_2_H3K27ac_S16_R1_001_RPGC_norm.bw \
+  ${bigwig_path}/CK0750_H3K27ac_E2_8_RPGC_norm.bw \
+  ${bigwig_path}/CK0751_H3K27ac_E2_9_RPGC_norm.bw \
+  ${bigwig_path}/PPT1_H3K27ac_S12_R1_001_RPGC_norm.bw \
+  ${bigwig_path}/PPT2_H3K27ac_S13_R1_001_RPGC_norm.bw \
+  ${bigwig_path}/PPT3_H3K27ac_S14_R1_001_RPGC_norm.bw \
    -R  ${prom_enh_path}/enhancers_HFDup_1500_anno.bed \
  ${prom_enh_path}/enhancers_HFDdown_681_anno.bed \
    -b 2000 -a 2000 \
